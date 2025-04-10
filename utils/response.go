54b486aa423a3dbde79e282b7130597de99e4be7
Jesse P Lesperance <jesse@jplesperance.me>
2025-04-10 10:07:45 -0700

    Add HttpResponse utility and update dependencies
    Introduced a `HttpResponse` struct and helper methods in `utils/response.go` to centralize HTTP response handling. Updated `go.mod` to include new dependencies necessary for improved functionality and future scalability.

---
+++ utils/response.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+)
+
+type HttpResponse struct {
+	Message    string      `json:"message,omitempty"`
+	Data       interface{} `json:"data,omitempty"`
+	StatusCode int         `json:"statusCode"`
+}
+
+func (e HttpResponse) Error() string {
+	return fmt.Sprintf("description: %s", e.Message)
+}
+
+func NewHttpResponse(statusCode int, message string, data interface{}) *HttpResponse {
+	return HttpResponse{
+		Message:    message,
+		Data:       data,
+		StatusCode: statusCode,
+	}
+}
