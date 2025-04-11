27ed9b94314087553339dcc873ed3029ad0a2a34
Jesse P Lesperance <jesse@jplesperance.me>
2025-04-10 17:45:09 -0700

    Add dotenv and API routing setup to main.go
    Integrated `godotenv` for environment variable management and set up basic API routing using Gin. Initialized database connections and registered authentication routes to the application.

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/jplesperance/passwordless-auth-system/db"
+	"github.com/jplesperance/passwordless-auth-system/module/auth"
+	"github.com/jplesperance/passwordless-auth-system/rdb"
+	"log"
+)
 
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	app := gin.Default()
+	api := app.Group("/api")
+	DB := db.Init()
+	RDB := rdb.Init()
+
+	auth.RegisterAuthRouter(api, DB, RDB)
+
+	app.Run(":8080")
 }
