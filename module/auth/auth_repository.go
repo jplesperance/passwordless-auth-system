91afb975825c5e3db770ba435dc7bbfd1d591206
Jesse P Lesperance <jesse@jplesperance.me>
2025-04-10 20:00:20 -0700

    Add method to fetch user by email in auth repository
    Introduce `FindFirstUserByEmail` method in the `AuthRepository` for querying users by email. Returns structured HTTP errors for not found and database-related issues.

--- module/auth/auth_repository.go
+++ module/auth/auth_repository.go
@@ -1,6 +1,10 @@
 package auth
 
 import (
+	"errors"
+
+	"github.com/jplesperance/passwordless-auth-system/db"
+	"github.com/jplesperance/passwordless-auth-system/utils"
 	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
@@ -11,6 +15,7 @@ type AuthRepository struct {
 }
 
 type IAuthRepository interface {
+	FindFirstUserByEmail(email string) (*db.User, error)
 }
 
 func NewAuthRepository(db *gorm.DB, rdb *redis.Client) IAuthRepository {
@@ -18,4 +23,18 @@ func NewAuthRepository(db *gorm.DB, rdb *redis.Client) IAuthRepository {
 		db:  db,
 		rdb: rdb,
 	}
+}
+
+func (AuthRepository *AuthRepository) FindFirstUserByEmail(email string) (db.User, error) {
+	var user db.User
+	err := AuthRepository.db.Model(&user).Where("email = ?", email).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return db.User{}, utils.NewHttpResponse(404, "User doesn't exist", nil)
+		} else {
+			return db.User{}, utils.NewHttpResponse(500, "Other db errors", err)
+		}
+	}
+
+	return user, nil
 }
